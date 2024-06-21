5b22bd0f810a6e750f6b1c5ec2e338845b4d9f68
Doug Bogle <dbogle@bowdoin.edu>
2024-06-21 01:30:10 +0000

    Added getting titles of uploaded videos capability, these titles are put in a list

--- youtubeproj.py
+++ youtubeproj.py
@@ -1,20 +1,45 @@
 import requests
+import pprint
+#FryingPan channel ID: UCfeMEuhdUtxtaUMNSvxq_Xg
 
 API_KEY = 'AIzaSyD9ZjC6u0PfNFtOswEZR-x2IvZ6dF3H-ec'
-CHANNEL_ID = 'UCfeMEuhdUtxtaUMNSvxq_Xg'
-PART = 'snippet'
-MAX_RESULTS = 10
+PART_CHANNEL = 'contentDetails'
+USERNAME = 'FryingPan'
 
-params = {
-  'part': PART,
-  'channelId': CHANNEL_ID,
+params_channel = {
+  'part': PART_CHANNEL,
+  'forHandle': USERNAME,
+  'key': API_KEY
+}
+CHANNEL_URL = 'https://www.googleapis.com/youtube/v3/channels'
+
+response = requests.get(CHANNEL_URL, params=params_channel)
+channel_info = response.json()
+
+ID = channel_info['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+PART_VIDS = 'snippet'
+MAX_RESULTS = 2
+
+params_playlistItems = {
+  'part': PART_VIDS,
+  'playlistId': ID,
   'maxResults': MAX_RESULTS,
   'key': API_KEY
 }
 
-BASE_URL = 'https://www.googleapis.com/youtube/v3/playlists'
+PLAYLISTS_URL = 'https://www.googleapis.com/youtube/v3/playlistItems'
 
-response = requests.get(BASE_URL, params=params)
-print(response.json())
-#works!
+upload_info = requests.get(PLAYLISTS_URL, params=params_playlistItems)
+uploads = upload_info.json()
+
+#pprint.pprint(uploads)
+
+titles = []
+
+for i in range(len(uploads['items'])):#successfully populates a list of titles!
+  #items is a list of every vid
+  titles.append(uploads['items'][i]['snippet']['title'])
+print(titles)
+#works nice!
+
 #git add, git commit, git push (will send it to remote once working)
