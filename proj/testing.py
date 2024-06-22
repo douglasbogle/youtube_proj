12190e09c2cb21a50eb6021d050049f066d2c7f9
Doug Bogle <dbogle@bowdoin.edu>
2024-06-22 19:01:56 +0000

    attempting to fix unit test

--- proj/testing.py
+++ proj/testing.py
@@ -1,5 +1,5 @@
 import unittest
-from youtubeproj import channel, search, populate_dict, videos
+from proj/youtubeproj import channel, search, populate_dict, videos
 
 class TestFileName(unittest.TestCase):
     def test_channel(self):
